ea15970884beb0f58761352b7eb4d508885676a1
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-12-30 16:32:54 -0400

    se instala dotenv y prueba , se vuelve a codigo original

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,9 +1,8 @@
-
+ 
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://seqahkdnzhhrqlolhtsl.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcWFoa2RuemhocnFsb2xodHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA5ODUzNDMsImV4cCI6MTk4NjU2MTM0M30.oMTAlY-7ryOZtHohZfp0821ICEeMQPJO1DoYsUYQ4oc"
-/* console.log(process.env.REACT_APP_SUPABASE_URL); */
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
