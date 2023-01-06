e55a9637e19d31a9594ca3714e960c9142d8b2c3
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-06 04:52:08 -0400

    prueba de uso de env variables de vercel

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,10 @@
  
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://seqahkdnzhhrqlolhtsl.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcWFoa2RuemhocnFsb2xodHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA5ODUzNDMsImV4cCI6MTk4NjU2MTM0M30.oMTAlY-7ryOZtHohZfp0821ICEeMQPJO1DoYsUYQ4oc"
+/* const supabaseUrl = "https://seqahkdnzhhrqlolhtsl.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcWFoa2RuemhocnFsb2xodHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA5ODUzNDMsImV4cCI6MTk4NjU2MTM0M30.oMTAlY-7ryOZtHohZfp0821ICEeMQPJO1DoYsUYQ4oc" */
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
