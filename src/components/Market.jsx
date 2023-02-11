b680ba6b05a5343ab77813f10dd2cab7ec01d309
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-11 13:58:42 -0400

    se implementa fetch de datos desde supabase en componente 'market'

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,16 +1,40 @@
 import { Box, Typography } from '@mui/material'
 import supabase from '../config/supabaseClient'
-
+import { useEffect, useState } from "react";
 import React from 'react'
 
 function Market() {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null)
+  const [datos, setDatos] = useState(null)
+  useEffect (() => {
+    const fetchData = async () => {
+      const {data, error} = await supabase
+      .from('users')
+      .select()
+
+      if (error) {
+        setFetchError("Unable to Fetch Data")
+        console.log(error);
+            }
+
+      if(data) {
+        setDatos(data)
+        setFetchError(null)
+      }      
+    }
+    fetchData()
+  }, [])
+  /* console.log(supabase); */
   return (
     <Box flex={4} p={1}>
-
-      <Typography variant="h4" sx={{ display: { xs: "none", sm: "block" } }}> MARKET </Typography>
-
-      <img src="" alt="" />
+      {fetchError && (<p>{fetchError}</p> ) }
+      {datos &&
+      <div>
+        {datos.map(user => (
+          <h3>{user.user_name}</h3>
+        ))}
+      </div> }
+     
 
     </Box>
   )
