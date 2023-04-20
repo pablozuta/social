a26a15991255a8d75c49cc38997e37d095b97862
pablozuta <pablo.zuta.h@gmail.com>
2023-04-20 02:22:08 -0400

    se agrega funcionalidad para hacer unfollow en pagina profile

--- src/blocks/FriendsCard.jsx
+++ src/blocks/FriendsCard.jsx
@@ -1,30 +1,41 @@
-import { Card } from '@mui/material'
-import React from 'react'
-import './friends-card.css'
+import { Card } from '@mui/material';
+import React, { useState } from 'react';
+import './friends-card.css';
 
 function FriendsCard(props) {
+  const [showCard, setShowCard] = useState(true);
+
+  const handleUnfollowClick = () => {
+    setShowCard(false);
+  };
+
+  if (!showCard) {
+    return null;
+  }
+
   return (
     <Card>
-    
       <div className="profile-card-followers">
         <div className="profile-card-avatar_followers">
-        <img src={props.image}
-          alt="" className="profile-avatar-followers" />
+          <img
+            src={props.image}
+            alt=""
+            className="profile-avatar-followers"
+          />
         </div>
-          <div className="profile-card-data">
-        <h4>{props.name}</h4>
-        <p>{props.handle}</p>
-        
-        <p>{props.bio}</p>
-      </div>
-
+        <div className="profile-card-data">
+          <h4>{props.name}</h4>
+          <p>{props.handle}</p>
+          <p>{props.bio}</p>
+        </div>
         <div className="profile-card-button">
-         <button><span className="label"></span></button>
+          <button onClick={handleUnfollowClick}>
+            <span className="label"></span>
+          </button>
         </div>
-        </div>
-     
+      </div>
     </Card>
-  )
+  );
 }
 
-export default FriendsCard
+export default FriendsCard;
